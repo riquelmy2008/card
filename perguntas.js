77ca2bb418f07d0ca0034a3b029eda873f98fe99
riquelmy2008 <138145196+riquelmy2008@users.noreply.github.com>
2024-09-30 14:02:28 -0300

    30/09

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'programação',              
+    'O que é Python?',
+   'O Python é uma linguagem de programação'  
+)
+ 
+criaCartao(
+'Geografia',
+'Qual capital da França?',
+'A capital da frança é Paris'
+)
+
+criaCartao(
+'Programação',
+'O que é função?',
+'Uma função ´é um bloco de código que executa alguma tarefa'
+)
+
+ criaCartao( 
+'Lingua inglsa',
+'Como se diz oi em Ingles?',
+'Oi em ingles é HI (RAI)'
+ )
